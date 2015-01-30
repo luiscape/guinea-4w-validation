b4dfcc11abd22e9992c15d259497f5df77fd8019
Luis Capelo <luiscape@gmail.com>
2015-01-30 18:04:04 -0500

    fixing file path

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Config
-PATH='tool/data/guinea-4w-data.csv'
+FILE_PATH='tool/data/guinea-4w-data.csv'
 CKAN_API_KEY='XXX'
 
 # Downloading and validating file
-~/R/bin/Rscript ~/tool/code/validation.R $PATH
+~/R/bin/Rscript ~/tool/code/validation.R $FILE_PATH
 
 # Creating / updating datastore
 source venv/bin/activate
-python ~/tool/code/create_datastore.py $CKAN_API_KEY $PATH
+python ~/tool/code/create_datastore.py $CKAN_API_KEY $FILE_PATH
